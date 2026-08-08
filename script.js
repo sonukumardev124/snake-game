8e10b71661e16873b212b6c10fd79e789f559a14
Sonu Kumar Gupta <sonukumar.devs124@gmail.com>
2026-08-08 09:15:08 +0530

    Move: one direction snake

--- script.js
+++ script.js
@@ -6,12 +6,35 @@ const cols = Math.floor(board.clientWidth / blockWidth);
 const rows = Math.floor(board.clientHeight / blockHeight);
 
 const blocks = []
+const snake = [{
+    x: 1, y: 3
+}]
+let direction = "right";
 
-for(let row = 0; row<rows; row++){
-    for(let col = 0; col<cols; col++){
+
+for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
         const block = document.createElement("div");
         block.classList.add("block");
         board.appendChild(block);
         blocks[`${row}-${col}`] = block;
     }
-} 
+}
+
+function render() {
+    snake.forEach(segment => {
+        blocks[`${segment.x}-${segment.y}`].classList.add("fill");
+    })
+}
+setInterval(() => {
+    let head = null;
+    if (direction === "right") {
+        head = { x: snake[0].x, y: snake[0].y + 1 }
+    }
+    snake.forEach(segment => {
+        blocks[`${segment.x}-${segment.y}`].classList.remove("fill");
+    });
+    snake.unshift(head);
+    snake.pop()
+    render()
+}, 400)
