488402c310a243d45b5b578a87893bce7e4fbfa9
Sonu Kumar Gupta <sonukumar.devs124@gmail.com>
2026-08-10 01:25:18 +0530

    fix: improve food positioning in snake game

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const blockWidth = 50;
 
 const cols = Math.floor(board.clientWidth / blockWidth);
 const rows = Math.floor(board.clientHeight / blockHeight);
+let intervalId = null;
+let food = { x: Math.floor(Math.random() * rows), y: Math.floor(Math.random() * cols) }
+
 const blocks = [];
 const snake = [{ x: 3, y: 4 }];
 let direction = "right";
@@ -18,12 +21,8 @@ for (let row = 0; row < rows; row++) {
     }
 }
 function render() {
-    snake.forEach(segment => {
-        blocks[`${segment.x}-${segment.y}`].classList.add("fill");
-    })
-}
-setInterval(() => {
     let head = null
+    blocks[`${food.x}-${food.y}`].classList.add("food")
     if (direction === "right") {
         head = { x: snake[0].x, y: snake[0].y + 1 }
     } else if (direction === "left") {
@@ -33,12 +32,29 @@ setInterval(() => {
     } else if (direction === "up") {
         head = { x: snake[0].x - 1, y: snake[0].y }
     }
+    if (head.x < 0 || head.x >= rows || head.y < 0 || head.y >= cols) {
+        alert("Game Over");
+        clearInterval(intervalId)
+    }
+    if (head.x == food.x && head.y == food.y) {
+        blocks[`${food.x}-${food.y}`].classList.remove("food")
+        food = { x: Math.floor(Math.random() * rows), y: Math.floor(Math.random() * cols) }
+        blocks[`${food.x}-${food.y}`].classList.add("food")
+        snake.unshift(head)
+    }
+
     snake.forEach(segment => {
         blocks[`${segment.x}-${segment.y}`].classList.remove("fill");
     })
 
     snake.unshift(head)
     snake.pop()
+    snake.forEach(segment => {
+        blocks[`${segment.x}-${segment.y}`].classList.add("fill");
+    })
+}
+intervalId = setInterval(() => {
+
     render()
 }, 400)
 
