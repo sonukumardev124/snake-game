274fcad149fe301d48e4c56eef95e495da30d870
Sonu Kumar Gupta <sonukumar.devs124@gmail.com>
2026-08-09 23:56:28 +0530

    control: Arrow function

--- script.js
+++ script.js
@@ -26,12 +26,30 @@ setInterval(() => {
     let head = null
     if (direction === "right") {
         head = { x: snake[0].x, y: snake[0].y + 1 }
+    } else if (direction === "left") {
+        head = { x: snake[0].x, y: snake[0].y - 1 }
+    } else if (direction === "down") {
+        head = { x: snake[0].x + 1, y: snake[0].y }
+    } else if (direction === "up") {
+        head = { x: snake[0].x - 1, y: snake[0].y }
     }
     snake.forEach(segment => {
         blocks[`${segment.x}-${segment.y}`].classList.remove("fill");
     })
- 
+
     snake.unshift(head)
     snake.pop()
     render()
 }, 400)
+
+addEventListener("keydown", (event) => {
+    if (event.key == "ArrowUp") {
+        direction = "up"
+    } else if (event.key == "ArrowLeft") {
+        direction = "left"
+    } else if (event.key == "ArrowRight") {
+        direction = "right"
+    } else if (event.key == "ArrowDown") {
+        direction = "down"
+    }
+})
